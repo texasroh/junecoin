c5da7a08a57320b613bf6cb18db7fc0e8bf87bf2
June Roh <texasroh@gmail.com>
2022-02-09 01:05:33 -0500

    init bolt db

--- db/db.go
+++ db/db.go
@@ -5,14 +5,26 @@ import (
 	"github.com/texasroh/junecoin/utils"
 )
 
+const (
+	dbName       = "blockchain.db"
+	dataBucket   = "data"
+	blocksBucket = "blocks"
+)
+
 var db *bolt.DB
 
 func DB() *bolt.DB {
 	if db == nil {
-		db, err := bolt.Open("blockchain.db", 0600, nil)
+		dbPointer, err := bolt.Open(dbName, 0600, nil)
+		db = dbPointer
 		utils.HandleErr(err)
-
-		db.dbPointer
+		err = db.Update(func(t *bolt.Tx) error {
+			_, err := t.CreateBucketIfNotExists([]byte(dataBucket))
+			utils.HandleErr(err)
+			_, err = t.CreateBucketIfNotExists([]byte(blocksBucket))
+			return err
+		})
+		utils.HandleErr(err)
 	}
 	return db
 }
