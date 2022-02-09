46aee3031ccf07776225708a3eea4c2adfd7eb7e
June Roh <texasroh@gmail.com>
2022-02-09 00:19:27 -0500

    added cli package

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/texasroh/junecoin
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	golang.org/x/sys v0.0.0-20220207234003-57398862261d // indirect
+)
--- main.go
+++ main.go
@@ -1,40 +1,7 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-
-	"github.com/texasroh/junecoin/explorer"
-	"github.com/texasroh/junecoin/rest"
-)
-
-func usage() {
-	fmt.Printf("Welcome to June coin\n\n")
-	fmt.Printf("Please use the following flags: \n\n")
-	fmt.Printf("-port=4000:   Set the PORT of the server\n")
-	fmt.Printf("-mode=rest:   Choose between 'html' and 'rest'\n\n")
-	os.Exit(0)
-}
+import "github.com/texasroh/junecoin/cli"
 
 func main() {
-
-	if len(os.Args) == 1 {
-		usage()
-	}
-
-	port := flag.Int("port", 4000, "Set port of the server")
-	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
-
-	flag.Parse()
-
-	switch *mode {
-	case "rest":
-		rest.Start(*port)
-	case "html":
-		explorer.Start(*port)
-	default:
-		usage()
-	}
-	fmt.Println(*port, *mode)
+	cli.Start()
 }
