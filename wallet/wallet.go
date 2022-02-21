440342063a852798350b3495a13c003722cb4cf4
June Roh <texasroh@gmail.com>
2022-02-21 03:14:10 -0500

    verifying messages

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -10,15 +10,19 @@ import (
 	"github.com/texasroh/junecoin/utils"
 )
 
+const (
+	hashedMessage string = "1c5863cd55b5a4413fd59f054af57ba3c75c0698b3851d70f99b8de2d5c7338f"
+)
+
 func Start() {
 	privateKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
 	utils.HandleErr(err)
 
-	message := "i love you"
-	hashedMessgae := utils.Hash(message)
-	hashAsBytes, err := hex.DecodeString(hashedMessgae)
+	hashAsBytes, err := hex.DecodeString(hashedMessage)
 	utils.HandleErr(err)
 	r, s, err := ecdsa.Sign(rand.Reader, privateKey, hashAsBytes)
+	signature := append(r.Bytes(), s.Bytes()...)
+	fmt.Printf("%x", signature)
 	utils.HandleErr(err)
-	fmt.Printf("R:%d\nS:%d", r, s)
+
 }
