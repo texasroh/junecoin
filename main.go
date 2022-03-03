f613dd1b3a53253af439e171984a5e0775067f26
June Roh <texasroh@gmail.com>
2022-03-03 01:58:42 -0500

    go routing and channel test

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"github.com/texasroh/junecoin/cli"
+	"fmt"
+	"time"
 )
 
+func countToTen(c chan<- int) {
+	for i := range [10]int{} {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("sending %d\n", i)
+		c <- i
+	}
+	close(c)
+}
+
+func receive(c <-chan int) {
+	for {
+		a, ok := <-c
+		if !ok {
+			fmt.Println("we are done.")
+			break
+		}
+		fmt.Printf("received %d\n", a)
+	}
+}
+
 func main() {
-	cli.Start()
+	// defer db.Close()
+	// cli.Start()
+	c := make(chan int)
+	go countToTen(c)
+	receive(c)
 }
